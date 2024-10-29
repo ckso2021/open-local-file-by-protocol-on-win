1bf2151867d7b95c10a95d76657228e088b1692e
CK So <sodo_sck@yahoo.com.hk>
2024-10-29 17:30:29 +0800

    fix space and special characters in path

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,46 @@ use std::{env, fs};
 use std::process::Command;
 use std::os::windows::process::CommandExt;
 use winapi::um::winbase::CREATE_NO_WINDOW;
-// use winapi::um::winbase::DETACHED_PROCESS;
 use urlencoding::decode;
-// use std::ptr;
-// use winapi::um::wincon::GetConsoleWindow;
-// use winapi::um::winuser::{ShowWindow, SW_HIDE};
 
 fn main() {
-   let args: Vec<String> = env::args().collect();
+    // hide_console_window();
+    // unsafe { winapi::um::wincon::FreeConsole() };
 
-   if args.len() <= 1 {
-      return;
-   }
+    let args: Vec<String> = env::args().collect();
 
-   let mut str = args[1].replace("openfile://", "");
-   str = decode(&str).expect("UTF-8").to_string();
+    if args.len() <= 1 {
+        return;
+    }
+
+    let mut str = args[1].replace("openfile://", "");
+    str = decode(&str).expect("UTF-8").to_string();
 
     match fs::metadata(&mut str) {
         Ok(_) => {
-         str = format!("{}", str);
-         // println!("Opening {}", str);
-         let mut binding = Command::new("cmd");
-         let command = binding.args(&["/C", &str]);
-         command.creation_flags(CREATE_NO_WINDOW); // Be careful: This only works on windows
-         command.spawn( ).unwrap( );
+            // handle space and special characters in path
+            let result = str.split("/").skip(1).map(|x| format!("\"{}\"", x)).collect::<Vec<String>>().join("/");
+            let drive = str.split("/").next().unwrap();
+
+            str = format!("/C {}/{}", drive, result);
+
+            let mut binding = Command::new("cmd");
+            let command = binding.raw_arg(str);
+            command.creation_flags(CREATE_NO_WINDOW); // Be careful: This only works on windows
+            command.spawn( ).unwrap( );
         },
         Err(_) => {
-         // println!("File does not exist!")
+            // println!("File does not exist!")
         },
     }
 }
+
+// fn hide_console_window() {
+//    let window = unsafe {GetConsoleWindow()};
+//    // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
+//    if window != ptr::null_mut() {
+//        unsafe {
+//            ShowWindow(window, SW_HIDE);
+//        }
+//    }
+// }
